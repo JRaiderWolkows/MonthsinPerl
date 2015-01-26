040019310fed0609eb8944a2f3df20d730c03d66
Your Name <jwolkows@gmail.com>
2015-01-26 15:27:52 -0500

    Commited sql database

---
+++ STA_Event.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 26, 2015 at 03:25 PM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `Adventure DB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `STA_Event`
+--
+
+CREATE TABLE IF NOT EXISTS `STA_Event` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID for Event',
+  `StateID` int(11) NOT NULL COMMENT 'StateID for Event',
+  `EventID` int(11) NOT NULL COMMENT 'EventID for Event',
+  `Order` int(11) NOT NULL COMMENT 'Order for Event',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
